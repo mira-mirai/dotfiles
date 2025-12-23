c7dd8e90ba8491807e0e4828b24ada15408797d1
mirai <mirahatesspam+git@proton.me>
2025-12-22 16:31:39 -0800

    update setup script to create symlinks

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # setup script for new computers/accounts
+
+# get dir for dotfiles repo and change to it
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" ))" &> /dev/null && pwd)
+ln -s $SCRIPT_DIR/.bashrc ~/.bashrc
+ln -s $SCRIPT_DIR/.bash_profile ~/.bash_profile
+ln -s $SCRIPT_DIR/.aliases ~/.aliases
